a63b3d0e8cf02ea180868dd672ea8bc5b7b05cec
Vongola <1085280247@qq.com>
2019-07-02 15:41:28 +0800

    体验servicework

---
+++ sw.js
@@ -0,0 +1,48 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! workbox is loaded 🎉`);
+  workbox.precaching.precacheAndRoute([
+    // 注册成功后要立即缓存的资源列表
+    './css/index.css'
+  ])
+  workbox.routing.registerRoute(
+    /.*\.css$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'css-cache',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp('.*\.html'),
+    new workbox.strategies.NetworkFirst({
+      cacheName: 'html-cache',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp('.*\.js'),
+    new workbox.strategies.CacheFirst({
+      cacheName: 'js-cache',
+    })
+  );
+  
+  workbox.routing.registerRoute(
+    //只缓存三找那个图片中2.png
+    new RegExp('2.png'),
+    new workbox.strategies.CacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // 只缓存1张图片
+          maxEntries: 1,
+          // 缓存时间100秒.
+          maxAgeSeconds: 100,
+        })
+      ],
+    })
+  );
+  
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
