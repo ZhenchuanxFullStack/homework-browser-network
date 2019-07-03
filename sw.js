05c19eb0415f8adb1a387867575cb6c9fd15e629
llxhz <1481010383@qq.com>
2019-07-03 20:15:44 +0800

    push to master: title: 异常捕捉 message：null

--- sw.js
+++ sw.js
@@ -1,31 +1,35 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+try {
+    importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-if(workbox) {
-    console.log(`Yay! Workbox is loaded 🎉`);
+    if(workbox) {
+        console.log(`Yay! Workbox is loaded 🎉`);
 
-    // html 请求优先
-    workbox.routing.registerRoute(
-        new RegExp('.*\.html'),
-        workbox.strategies.networkFirst()
-    );
+        // html 请求优先
+        workbox.routing.registerRoute(
+            new RegExp('.*\.html'),
+            workbox.strategies.networkFirst()
+        );
 
-    // js css 没有版本号 staleWhileRevalidate
-    workbox.routing.registerRoute(
-        new RegExp('.*\.(?:js|css)'),
-        workbox.strategies.staleWhileRevalidate()
-    );
+        // js css 没有版本号 staleWhileRevalidate
+        workbox.routing.registerRoute(
+            new RegExp('.*\.(?:js|css)'),
+            workbox.strategies.staleWhileRevalidate()
+        );
 
-    // 图片缓存优先
-    workbox.routing.registerRoute(
-        new RegExp('.*\.(png|gif|ico)'),
-        workbox.strategies.cacheFirst()
-    )
+        // 图片缓存优先
+        workbox.routing.registerRoute(
+            new RegExp('.*\.(png|gif|ico)'),
+            workbox.strategies.cacheFirst()
+        )
 
-    // 强制jpg图片始终走请求 测试
-    workbox.routing.registerRoute(
-        new RegExp('.*\.jpg'),
-        workbox.strategies.networkOnly()
-    )
-} else {
-    console.log(`Boo! Workbox didn't load 😬`);
+        // 强制jpg图片始终走请求 测试
+        workbox.routing.registerRoute(
+            new RegExp('.*\.jpg'),
+            workbox.strategies.networkOnly()
+        )
+    } else {
+        console.log(`Boo! Workbox didn't load 😬`);
+    }
+} catch (e) {
+    console.log(e);
 }
