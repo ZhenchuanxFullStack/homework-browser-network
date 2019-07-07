6ee2e04f3db9eae8391bd3aa0e4535b2150a49a6
jiosers <jioser@qq.com>
2019-07-07 15:01:08 +0800

    add explain and modify bug

--- sw.js
+++ sw.js
@@ -1,34 +1,34 @@
 // 首先引入 Workbox 框架
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js');
-if(!workbox) {
+if(workbox) {
+    // 静态资源缓存
+    workbox.precaching.preacheAndRoute([
+        '/js/test.js',
+        '/imgs/1.jpeg'
+    ])
+
+    // html的缓存策略
+    workbox.routing.registerRoute(
+        new RegExp('.*\.html'),
+        workbox.strategies.networkFirst()
+    )
+
+    workbox.routing.registerRoute(
+        new RegExp('.*\.(?:js|css)'),
+        workbox.strategies.cacheFirst()
+    );
+    
+    workbox.routing.registerRoute(
+        new RegExp('https://your\.cdn\.com/'),
+        workbox.strategies.staleWhileRevalidate()
+    );
+    
+    workbox.routing.registerRoute(
+        new RegExp('https://your\.img\.cdn\.com/'),
+        workbox.strategies.cacheFirst({
+            cacheName: 'example:img'
+        })
+    );
+} else {
     console.log(`workbox is didn't load`);
-    return;
 }
-// 静态资源缓存
-workbox.precaching.preacheAndRoute([
-    '/js/test.js',
-    '/imgs/1.jpeg'
-])
-
-// html的缓存策略
-workbox.routing.registerRoute(
-    new RegExp('.*\.html'),
-    workbox.strategies.networkFirst()
-)
-
-workbox.routing.registerRoute(
-    new RegExp('.*\.(?:js|css)'),
-    workbox.strategies.cacheFirst()
-);
-  
-workbox.routing.registerRoute(
-    new RegExp('https://your\.cdn\.com/'),
-    workbox.strategies.staleWhileRevalidate()
-);
-  
-workbox.routing.registerRoute(
-    new RegExp('https://your\.img\.cdn\.com/'),
-    workbox.strategies.cacheFirst({
-        cacheName: 'example:img'
-    })
-);
